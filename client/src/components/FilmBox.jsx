dbbddedf4af521e80d8399a6ea781e3c6865cd0a
Robert Ball <robert.ball.1989@gmail.com>
2017-05-01 21:05:32 +0100

    Removed semicolons from source code and added data to film box state.

--- client/src/components/FilmBox.jsx
+++ client/src/components/FilmBox.jsx
@@ -6,12 +6,23 @@ class FilmBox extends React.Component{
 
   constructor(props){
     super(props)
+    this.state = {
+      data: [
+      {
+        id:1,
+        title: "A Cat's Porpoise"
+      },
+      {
+        id:2,
+        title: "Sleepful"
+      }]
+    }
   }
 
   render(){
     return(
         <div className="film-box">
-          this is the film box
+          <p>this is the film box</p>
         </div>
       )
   }
